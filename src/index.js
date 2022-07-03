1b4d7d6b5c674320b701052f686c138927fa1761
stevehu <stevehuinnz@gmail.com>
2022-07-03 20:10:26 +1200

    first day

--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+class App extends React.Component {
+    state = {
+        txt: ''
+    }
+    handleChange = e => {
+        this.setState({
+            txt: e.target.value
+        })
+    }
+    render() {
+        return ( < div > < input type = "text"
+            value = { this.state.txt }
+            onChange = { this.handleChange }
+            /> <
+            br / >
+            <
+            textarea > < /textarea> <
+            br / >
+
+
+
+            <
+            /div >
+        )
+
+    }
+
+
+
+
+
+
+}
+
+
+
+ReactDOM.render( < App / > , document.getElementById('root'))
